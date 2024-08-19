8efa2d514888cbc36e896adb6d58e329e7d939c1
etvorellana <evalero@uesc.br>
2024-08-19 18:05:44 -0300

    Exemplos da Aula T01 e T02

---
+++ Aula-T02/Main_04.java
@@ -0,0 +1,24 @@
+public class Main_04 {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        int a = 10;
+        int b = a;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        a = 20;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        // Reference Data Types
+        String yourName = "Who";
+        String myName = yourName;
+        System.out.println("yourName: " + yourName);
+        System.out.println("myName: " + myName);
+        yourName = "You";
+        System.out.println("yourName: " + yourName);
+        System.out.println("myName: " + myName);
+        System.out.println(yourName == myName);
+
+    }
+}
+
+
